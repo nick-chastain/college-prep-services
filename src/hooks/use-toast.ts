60a35271a7f3e649f97d4750eaad29aadb2c8cc2
malcolm-macdonald <117494231+malcolm-macdonald@users.noreply.github.com>
2025-03-22 23:08:15 -0600

    Fix useToast hook to resolve build error

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -6,10 +6,6 @@ import type {
 } from "@/components/ui/toast"
 
 import { Toast } from "@/components/ui/toast";
-import {
-  useToast as useToastOriginal,
-  ToasterToast
-} from "@/components/ui/use-toast";
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
